41b09a3c9dab122059f315f4c35afdf4253847b4
Taras Chornenkiy <taraschrt06@gmail.com>
2016-08-23 11:31:48 +0300

    libs deleted

--- hw/mcu/stm/stm32l1xx/include/mcu/cortex_m3.h
+++ hw/mcu/stm/stm32l1xx/include/mcu/cortex_m3.h
@@ -17,34 +17,11 @@
  * under the License.
  */
 
-#include <assert.h>
-#include <stddef.h>
-#include "testutil/testutil.h"
-#include "os/os_test.h"
-#include "os_test_priv.h"
+#ifndef __MCU_CORTEX_M3_H__
+#define __MCU_CORTEX_M3_H__
 
-int
-os_test_all(void)
-{
-    os_mempool_test_suite();
-    os_mutex_test_suite();
-    os_sem_test_suite();
-    os_mbuf_test_suite();
+#include "mcu/stm32l1xx.h"
 
-    return tu_case_failed;
-}
+#define OS_TICKS_PER_SEC    (1000)
 
-#ifdef MYNEWT_SELFTEST
-
-int
-main(int argc, char **argv)
-{
-    tu_config.tc_print_results = 1;
-    tu_init();
-
-    os_test_all();
-
-    return tu_any_failed;
-}
-
-#endif
+#endif /* __MCU_CORTEX_M3_H__ */
